50e427c887122ab90739b74c870a5895852bfe43
Rachel Nabors <rachelnabors@gmail.com>
2015-02-20 12:55:39 -0800

    Made a bunch of changes at Rodney Rehm's advice. This should be more scalable and legible.

--- js/core.js
+++ js/core.js
@@ -1,276 +1,260 @@
 (function(){
-"use strict";
+	"use strict";
 
-var body = document.body;
-var character = document.getElementById("character");
-var controls = document.getElementById("controls");
-var buttons = controls.getElementsByTagName("button");
-var winStateCTA = document.getElementById("winState");
-var loseStateCTA = document.getElementById("loseState");
-var lastInteractionImpact, lastMood, depreciation;
+	var body = document.body;
+	var character = document.getElementById("character");
+	var controls = document.getElementById("controls");
+	var buttons = [].slice.call(controls.getElementsByTagName("button"), 0);
+	var winStateCallToAction = document.getElementById("winState");
+	var loseStateCallToAction = document.getElementById("loseState");
+	var depletionInterval = 5000;
+	var lastInteractionImpact, lastMood, depreciationTimer, userAway;
 
-// Characters's Happiness Meters
-var happinessMeter = {
-    // (They all start at 1. Your work is cut out for you!)
-    meters : {
-        restMeter : 1,
-        hungerMeter : 1,
-        engagementMeter : 1
-    },
-    hungerMeterViz : document.getElementById("meter-hunger"),
-    restMeterViz : document.getElementById("meter-rest"),
-    engagementMeterViz : document.getElementById("meter-engagement"),
-    measureHappiness : function() {
-        var sumHappiness = 0;
-        for (var i in happinessMeter.meters) {
-            sumHappiness = sumHappiness + happinessMeter.meters[i];
-        }
-        return sumHappiness;
-    },
-    updateMeterViz : function(meter, points) {
-        meter.value = points;
-        meter.setAttribute("aria-valuenow", points);
-    },
-    incrementPoint : function(meterName) {
-        if (this.meters[meterName] < 3) {
-          this.meters[meterName]++;
-        }
-        this.updateMeterViz(happinessMeter[meterName + "Viz"], this.meters[meterName]);
+	// Characters's Happiness Meters
+	var happinessMeter = {
+		// (They all start at 1. Your work is cut out for you!)
+		meters : {
+			rest : 1,
+			hunger : 1,
+			engagement : 1
+		},
+		visibleMeters: {
+			hunger : document.getElementById("meter-hunger"),
+			rest : document.getElementById("meter-rest"),
+			engagement : document.getElementById("meter-engagement")
+		},
+		measureHappiness : function() {
+			var sumHappiness = 0;
+			for (var i in happinessMeter.meters) {
+				sumHappiness = sumHappiness + happinessMeter.meters[i];
+			}
+			return sumHappiness;
+		},
+		updateVisibleMeter : function(meter, newValue) {
+			meter.value = newValue;
+			meter.setAttribute("aria-valuenow", newValue);
+		},
+		incrementMeter : function(meterName, amount) {
+			if (this.meters[meterName] + amount >= 3) {
+			  this.meters[meterName] = 3;
+			} else {
+				this.meters[meterName] += amount;
+			}
+			this.updateVisibleMeter(this.visibleMeters[meterName], this.meters[meterName]);
 
-    },
-    decrementPoint : function(meterName) {
-        if (this.meters[meterName] > 0) {
-          this.meters[meterName]--;
-        }
-        this.updateMeterViz(happinessMeter[meterName + "Viz"], this.meters[meterName]);
-    },
-    init : function() {
-        controls.hidden = false;
-        winStateCTA.hidden = true;
-        loseStateCTA.hidden = true;
-        // enable buttons
-        for (var i=0; i<buttons.length; i++) {
-            buttons[i].removeAttribute("disabled");
-        }
-        // Measure the meters and set them up in the DOM
-        for (var i in happinessMeter.meters) {
-            happinessMeter.meters[i] = 1;
-            happinessMeter.updateMeterViz(happinessMeter[i +"Viz"], happinessMeter.meters[i]);
-        }
-        updateEmotion();
-        // Start happiness depletion timer
-        depreciation = setInterval(depreciateMeters, 5000);
-    },
-    youWin : function() {
-        // stop depreciation
-        clearInterval(depreciation);
-        // hide controls, show win state
-        controls.hidden = true;
-        winStateCTA.hidden = false;
-    },
-    youLose : function() {
-        // stop depreciation
-        clearInterval(depreciation);
-        // hide controls, show lose state
-        for (var i=0; i<buttons.length; i++) {
-            buttons[i].setAttribute("disabled", true);
-        }
-        loseStateCTA.hidden = false;
-    }
-}
+		},
+		decrementMeter : function(meterName, amount) {
+			if (this.meters[meterName] - amount <= 0) {
+			  this.meters[meterName] = 0;
+			} else {
+				this.meters[meterName] -= amount;
+			}
+			this.updateVisibleMeter(this.visibleMeters[meterName], this.meters[meterName]);
+		},
+		init : function() {
+			controls.hidden = false;
+			winStateCallToAction.hidden = true;
+			loseStateCallToAction.hidden = true;
+			// enable buttons: iterate over the array of buttons with forEach
+			[].forEach.call(buttons, function(element) {
+				element.removeAttribute("disabled");
+			});
 
-function swapClasses(oldClass, newClass) {
-    body.classList.remove(oldClass);
-    body.classList.add(newClass);
-    return newClass;
-}
+			// Measure the meters and set them up in the DOM
+			Object.keys(this.meters).forEach(function(key) {
+				happinessMeter.meters[key] = 1;
+				happinessMeter.updateVisibleMeter(happinessMeter.visibleMeters[key], happinessMeter.meters[key]);
+			});
+			updateEmotion();
+			// Start happiness depletion timer
+			depreciationTimer = setTimeout(depreciateMeters, depletionInterval);
+		},
+		youWin : function() {
+			setTimeout( function(){
+				// stop depreciation
+				clearTimeout(depreciationTimer);
+				// hide controls, show win state
+				controls.hidden = true;
+				winStateCallToAction.hidden = false;
+			}, 1750)
+		},
+		youLose : function() {
+			// stop depreciation
+			clearTimeout(depreciationTimer);
+			setTimeout( function(){
+				// hide controls, show lose state
+				[].forEach.call(buttons, function(element) {
+					element.setAttribute("disabled", true);
+				});
+				loseStateCallToAction.hidden = false;
+			}, 1750)
+		}
+	}
 
-//from the fail state
-document.getElementById("tryAgain").addEventListener("click", function(){
-    happinessMeter.init();
-    controls.classList.remove("deactivated");
-});
+	function swapClasses(oldClass, newClass) {
+		body.classList.remove(oldClass);
+		body.classList.add(newClass);
+		return newClass;
+	}
 
-// from the win state
-document.getElementById("playAgain").addEventListener("click", function(){
-    happinessMeter.init();
-});
+	// Update Action character takes
+	function updateAction(actionTaken, repercussions) {
+		body.classList.add(actionTaken);
+		// stop the clock.
+		clearTimeout(depreciationTimer);
+			console.log("cleared");
+		// Plz don't hit the buttons
+		[].forEach.call(buttons, function(element) {
+			element.setAttribute("disabled", true);
+		});
+		// wait 5 seconds.
+		setTimeout(function(){
+			// remove action class.
+			body.classList.remove(actionTaken);
+			// turn buttons back on
+			[].forEach.call(buttons, function(element) {
+				element.removeAttribute("disabled");
+			});
+			// restart the clock
+			depreciationTimer = setTimeout(depreciateMeters, depletionInterval);
+			repercussions();
+		}, depletionInterval);
+	}
 
-// Buttons add happiness points... and take them away.
-document.getElementById("meal").addEventListener("click", function(){
-    updateAction("eating", function(){
-        happinessMeter.incrementPoint("hungerMeter");
-        happinessMeter.incrementPoint("hungerMeter");
-        happinessMeter.decrementPoint("restMeter");
-        updateEmotion("full");
-    });
-});
+	// Update Character's mood
+	function updateEmotion(interactionImpact) {
+		var totalPoints = happinessMeter.measureHappiness();
 
-document.getElementById("snack").addEventListener("click", function(){
-    updateAction("snacking", function(){
-        happinessMeter.incrementPoint("hungerMeter");
-        happinessMeter.decrementPoint("engagementMeter");
-        updateEmotion("yum");
-    });
-});
 
-document.getElementById("sleep").addEventListener("click", function(){
-    updateAction("sleeping", function(){
-        happinessMeter.incrementPoint("restMeter");
-        happinessMeter.incrementPoint("restMeter");
-        updateEmotion("rested");
-    });
-});
+		// find out how they feel
+		// act accordingly
+		var moods = ["rage-quit", "enraged", "unhappy", "okay", "okay", "happy", "happy", "joyous", "joyous", "overjoyed"];
+		lastMood = swapClasses(lastMood, moods[totalPoints]);
 
-document.getElementById("play").addEventListener("click", function(){
-    updateAction("playing", function(){
-        happinessMeter.incrementPoint("engagementMeter");
-        happinessMeter.decrementPoint("restMeter");
-        updateEmotion("rambunctious");
-    });
-});
+		// if no points, lose. If 9 points, win!
+		if (!totalPoints) {
+		  happinessMeter.youLose();
+		} else if (totalPoints === 9) {
+		  happinessMeter.youWin();
+		}
 
-document.getElementById("read").addEventListener("click", function(){
-    updateAction("reading", function(){
-        happinessMeter.incrementPoint("engagementMeter");
-        updateEmotion("curious");
-    });
-});
+		// these always take precedence over the above in the CSS
+		function updateNeeds(meter, need) {
+			// https://developer.mozilla.org/en-US/docs/Web/API/Element.classList
+			body.classList.toggle(need, !meter);
+		}
 
-// Update Action character takes
-function updateAction(actionTaken, repercussions) {
-    body.classList.add(actionTaken);
-    // stop the clock.
-    clearInterval(depreciation);
-    // Plz don't hit the buttons
-    for (var i=0; i<buttons.length; i++) {
-        buttons[i].setAttribute("disabled", true);
-    }
+		updateNeeds(happinessMeter.meters.engagement, "bored");
+		updateNeeds(happinessMeter.meters.hunger, "hungry");
+		updateNeeds(happinessMeter.meters.rest, "tired");
 
-    // wait 5 seconds.
-    var fallout = setTimeout(function(){
-        clearTimeout(fallout);
-        // remove action class.
-        body.classList.remove(actionTaken);
-        // turn buttons back on
-        for (var i=0; i<buttons.length; i++) {
-            buttons[i].removeAttribute("disabled");
-        }
-        // restart the clock
-        depreciation = setInterval(depreciateMeters, 5000);
-        repercussions();
-    }, 5000);
-}
+		// Show a special emotion after each action
+		// By adding a new class
+		if (interactionImpact) {
+			lastInteractionImpact = swapClasses(lastInteractionImpact, interactionImpact);
+		}
+		// This allows our character to be happy about something even if it over all makes them sad.
+	}
 
-// Update Character's mood
-function updateEmotion(interactionImpact) {
-    var totalPoints = happinessMeter.measureHappiness();
+	function hasValue(obj, key, value) {
+		return obj.hasOwnProperty(key) && obj[key] === value;
+	}
 
-    // find out how they feel
-    // act accordingly
-    if (totalPoints == 0){
-        console.log("That's it! I'm outta here!");
-        lastMood = swapClasses(lastMood, "rage-quit");
-        happinessMeter.youLose();
-    } else if (totalPoints <= 1){
-        console.log("VERY unhappy");
-        lastMood = swapClasses(lastMood, "enraged");
-    } else if (totalPoints <= 2){
-        console.log("unhappy");
-        lastMood = swapClasses(lastMood, "unhappy");
-    } else if (totalPoints <= 4) {
-        console.log("okay");
-        lastMood = swapClasses(lastMood, "okay");
-    } else if (totalPoints <= 6) {
-        console.log("happy");
-        lastMood = swapClasses(lastMood, "happy");
-    } else if (totalPoints == 8) {
-        console.log("So happy!");
-        lastMood = swapClasses(lastMood, "joyous");
-    } else if (totalPoints == 9) {
-        console.log("You win at life!!");
-        lastMood = swapClasses(lastMood, "overjoyed");
-        happinessMeter.youWin();
-    }
+	function depreciateMeters() {
+    	var meterArray = Object.keys(happinessMeter.meters);
+    	var lengthOfMeters = meterArray.length;
+    	var randomMeter = Math.floor(Math.random() * (1 - lengthOfMeters)) + lengthOfMeters;
+    	var randomMeterName = meterArray[randomMeter];
 
-    // these always take precedence over the above in the CSS
-    function updateNeeds(meter, need) {
-        if (meter > 0) {
-            body.classList.remove(need);
-        } else {
-            body.classList.add(need);
-        }
-    }
+    	// find out if we have more than one points left
+		if (happinessMeter.measureHappiness() > 1) {
+			// randomly pick a meter that has some points left
+			if (hasValue(happinessMeter.meters, randomMeterName, 0)) {
+				// bleh, there's nothing in this meter, start again
+				depreciateMeters();
+			} else {
+				// ooh, this meter has something...
+				happinessMeter.decrementMeter(randomMeterName, 1);
+				updateEmotion();
+				depreciationTimer = setTimeout(depreciateMeters, depletionInterval);
+			}
+		} else {
+			// You have only one point left.
+			Object.keys(happinessMeter.meters).forEach(function(key) {
+				// Set all happinessMeter's meters to 0.
+				happinessMeter.meters[key] = 0;
+				// Update the visual meters.
+				happinessMeter.updateVisibleMeter(happinessMeter.visibleMeters[key], happinessMeter.meters[key]);
+			});
+			updateEmotion();
+			console.log("Game over, man, game over!")
+		}
+	};
 
-    updateNeeds(happinessMeter.meters.engagementMeter, "bored");
-    updateNeeds(happinessMeter.meters.hungerMeter, "hungry");
-    updateNeeds(happinessMeter.meters.restMeter, "tired");
+	//from the fail state
+	document.getElementById("tryAgain").addEventListener("click", function(){
+		happinessMeter.init();
+		controls.classList.remove("deactivated");
+	});
 
-    // Show a special emotion after each action
-    // By adding a new class
-    if (interactionImpact) {
-        lastInteractionImpact = swapClasses(lastInteractionImpact, interactionImpact);
-    }
-    // This allows our character to be happy about something even if it over all makes them sad.
-}
+	// from the win state
+	document.getElementById("playAgain").addEventListener("click", function(){
+		happinessMeter.init();
+	});
 
-function hasValue(obj, key, value) {
-    return obj.hasOwnProperty(key) && obj[key] === value;
-}
+	// Buttons add happiness points... and take them away.
+	document.getElementById("meal").addEventListener("click", function(){
+		updateAction("eating", function(){
+			happinessMeter.incrementMeter("hunger", 2);
+			happinessMeter.decrementMeter("rest", 1);
+			updateEmotion("full");
+		});
+	});
 
-function pickRandomProperty(obj) {
-    var result;
-    var count = 0;
-    for (var prop in obj)
-        if (Math.random() < 1/++count)
-           result = prop;
-    return result;
-}
+	document.getElementById("snack").addEventListener("click", function(){
+		updateAction("snacking", function(){
+			happinessMeter.incrementMeter("hunger", 1);
+			happinessMeter.decrementMeter("engagement", 1);
+			updateEmotion("yum");
+		});
+	});
 
-function depreciateMeters() {
-    // find out if we have more than one points left
-    var randomMeter = pickRandomProperty(happinessMeter.meters);
-    var meterValue, meterViz;
+	document.getElementById("sleep").addEventListener("click", function(){
+		updateAction("sleeping", function(){
+			happinessMeter.incrementMeter("rest", 2);
+			updateEmotion("rested");
+		});
+	});
 
-    if (happinessMeter.measureHappiness() > 1) {
-        // randomly pick a meter that has some points left
-        if (hasValue(happinessMeter.meters, randomMeter, 0)) {
-            // bleh, there's nothing in this meter, start again
-            depreciateMeters();
-        } else {
-            // ooh, this meter has something...
-            meterValue = happinessMeter.meters[randomMeter];
-            meterViz = randomMeter + "Viz";
-            happinessMeter.decrementPoint(randomMeter);
-            updateEmotion();
-        }
-    } else {
-        // You have only one point left.
-        for (var i in happinessMeter.meters) {
-            // Set all happinessMeter's meters to 0.
-            happinessMeter.meters[i] = 0;
-            // Update the visual meters.
-            happinessMeter.updateMeterViz(happinessMeter[i +"Viz"], happinessMeter.meters[i]);
-        }
-        updateEmotion();
-        console.log("Game over, man, game over!")
-    }
-};
+	document.getElementById("play").addEventListener("click", function(){
+		updateAction("playing", function(){
+			happinessMeter.incrementMeter("engagement", 1);
+			happinessMeter.decrementMeter("rest", 1);
+			updateEmotion("rambunctious");
+		});
+	});
 
-// Music
-// Get the audio element
-// var soundTyping = document.querySelector('audio');
+	document.getElementById("read").addEventListener("click", function(){
+		updateAction("reading", function(){
+			happinessMeter.incrementMeter("engagement", 1);
+			updateEmotion("curious");
+		});
+	});
 
-// // Wait till the music loads (or get a nasty error!)
-// soundTyping.addEventListener("canplaythrough", function () {
+	// Music
+	// Get the audio element
+	// var soundTyping = document.querySelector('audio');
 
-// }, false);
+	// // Wait till the music loads (or get a nasty error!)
+	// soundTyping.addEventListener("canplaythrough", function () {
 
-window.addEventListener("load", function load(event){
-    window.removeEventListener("load", load, false); //remove listener, no longer needed
+	// }, false);
 
-    // start the happiness meter afresh
-    happinessMeter.init();
-},false);
+	window.addEventListener("load", function load(event){
+		window.removeEventListener("load", load, false); //remove listener, no longer needed
+
+		// start the happiness meter afresh
+		happinessMeter.init();
+	},false);
 
 })();
